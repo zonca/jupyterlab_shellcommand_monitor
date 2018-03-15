686beb4c4d8e0f3e484a21e813c310cbd313d68b
Andrea Zonca <code@andreazonca.com>
2018-03-15 04:04:49 -0400

    call any bash command server extension

---
+++ jup_mon.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler
+
+def run_command(cmd):
+    try:
+        return subprocess.run(cmd,
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            check=True, timeout=60, encoding="utf-8")
+    except subprocess.CalledProcessError as e:
+        print("Error:", e.stderr)
+        raise
+
+class ExecuteShellHandler(IPythonHandler):
+    def get(self, command):
+        answer = run_command(command.strip().split("/"))
+        self.finish(answer.stdout.strip())
+
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    web_app = nb_server_app.web_app
+    host_pattern = '.*$'
+    route_pattern = url_path_join(web_app.settings['base_url'], '/shell/(.*)')
+    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
