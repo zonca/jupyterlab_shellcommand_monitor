592e62fa5a7431b230d7523f822872ef60fc049b
Andrea Zonca <code@andreazonca.com>
2018-03-15 03:58:28 -0400

    completed xkcd tutorial

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
 import '../style/index.css';
 
 
@@ -11,8 +19,42 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab_slurm',
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension jupyterlab_slurm is activated!');
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
+  // Create a single widget
+  let widget: Widget = new Widget();
+  widget.id = 'xkcd-jupyterlab';
+  widget.title.label = 'xkcd.com';
+  widget.title.closable = true;
+  // Add an image element to the panel
+  let img = document.createElement('img');
+  widget.node.appendChild(img);
+
+  // Fetch info about a random comic
+  fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+    return response.json();
+  }).then(data => {
+    img.src = data.img;
+    img.alt = data.title;
+    img.title = data.alt;
+  });
+
+  // Add an application command
+  const command: string = 'xkcd:open';
+  app.commands.addCommand(command, {
+    label: 'Random xkcd comic',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.addToMainArea(widget);
+      }
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({command, category: 'Tutorial'});
   }
 };
 
