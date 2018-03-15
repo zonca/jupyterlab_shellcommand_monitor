97eadd420ff931363014f382404341fba1e17da4
Andrea Zonca <code@andreazonca.com>
2018-03-15 05:45:07 -0400

    [bugfix] fix handler name

--- jup_mon.py
+++ jup_mon.py
@@ -32,4 +32,4 @@ def load_jupyter_server_extension(nb_server_app):
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
     route_pattern = url_path_join(web_app.settings['base_url'], '/shell/(.*)')
-    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
+    web_app.add_handlers(host_pattern, [(route_pattern, ExecuteShellHandler)])
